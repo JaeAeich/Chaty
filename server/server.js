575779881e8273a968213a0c1a6d9b54de743944
Javed Habib <jh4official@gmail.com>
2023-07-15 15:24:33 +0530

    feat: Setup basic GET API routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const chats = require('../data/data');  // Dummy data
 
 const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Welcome to chaty API!');
+});
+
+app.get('/api/chat', (req, res) => {
+	console.log(chats);
+	res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+	let id = req.params['id'];
+	res.send(id);
+});
 
 app.listen(5000, console.log('Hello from server!'));
