3376505f924ccca546deafb1559f4a4388b842e6
Javed Habib <jh4official@gmail.com>
2023-07-15 17:21:42 +0530

    feat: Add routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,16 @@
-export default function App() {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+import { Routes, Route } from 'react-router-dom';
+import ChatPage from './Pages/ChatPage';
+import LandingPage from './Pages/LandingPage';
+
+function App() {
+	return (
+		<div className='text-3xl font-bold underline'>
+			<Routes>
+				<Route path='/' exact element={<LandingPage />} />
+				<Route path='/chat' exact element={<ChatPage />} />
+			</Routes>
+		</div>
+	);
 }
+
+export default App;
