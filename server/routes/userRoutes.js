3ade8e2ba8659d0edb43925335413b94556c4a9f
Javed Habib <jh4official@gmail.com>
2023-07-17 23:17:22 +0530

    feat: Add routers

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('Express');
+
+const router = express.router;
+
+// router.post('/', registerUser);
+// router.post('/login', authUser);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const chats = require('./data/data'); // Dummy data
 const { connectDB } = require('./config/db');
+const userRotues = require('./routes/userRoutes')
 
 const app = express();
 // Configure environment variables
@@ -12,15 +13,7 @@ app.get('/', (req, res) => {
 	res.send('Welcome to chaty API!');
 });
 
-app.get('/api/chat', (req, res) => {
-	console.log(chats);
-	res.send(chats);
-});
-
-app.get('/api/chat/:id', (req, res) => {
-	let id = req.params['id'];
-	res.send(id);
-});
+app.use('/api/user', userRotues);
 
 const port = process.env.PORT;
 
