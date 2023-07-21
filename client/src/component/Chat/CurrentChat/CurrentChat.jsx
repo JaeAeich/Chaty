69b1ca075747616795ef1a92ff064cf8b96462ea
Javed Habib <jh4official@gmail.com>
2023-07-21 12:13:49 +0530

    feat: Add static messaging

--- client/src/component/Chat/CurrentChat/CurrentChat.jsx
+++ client/src/component/Chat/CurrentChat/CurrentChat.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChatState } from '../../../context/chatProvider';
 import { getSender, getSenderFull } from '../../../config/chatConfig';
 import Modal from 'react-modal';
 import ProfileModal from '../ProfileModal/ProfileModal';
 import './CurrentChat.css';
 import GroupInfoModal from '../GroupInfoModal/GroupInfoModal';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import CurrentScrollableChat from '../CurrentScrollableChat/CurrentScrollableChat';
 
 const CurrentChat = ({ fetchAgain, setFetchAgain }) => {
 	const [messages, setMessages] = useState([]);
@@ -34,23 +37,99 @@ const CurrentChat = ({ fetchAgain, setFetchAgain }) => {
 			setLoading(true);
 
 			const { data } = await axios.get(
-				`/api/message/${selectedChat._id}`,
+				`${import.meta.env.VITE_BACKEND_BASE_URL}/api/message/${
+					selectedChat._id
+				}`,
 				config
 			);
 			setMessages(data);
 			setLoading(false);
 
-			socket.emit('join chat', selectedChat._id);
+			// socket.emit('join chat', selectedChat._id);
 		} catch (error) {
-			toast({
-				title: 'Error Occured!',
-				description: 'Failed to Load the Messages',
-				status: 'error',
-				duration: 5000,
-				isClosable: true,
-				position: 'bottom',
-			});
+			toast.error('Error Occured!');
 		}
+	};
+
+	const sendMessage = async (event) => {
+		if (event.key === 'Enter' && newMessage) {
+			event.preventDefault();
+			// socket.emit('stop typing', selectedChat._id);
+			try {
+				const config = {
+					headers: {
+						'Content-type': 'application/json',
+						Authorization: `Bearer ${user.token}`,
+					},
+				};
+				setNewMessage('');
+				const { data } = await axios.post(
+					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/message`,
+					{
+						content: newMessage,
+						chatId: selectedChat,
+					},
+					config
+				);
+				// socket.emit('new message', data);
+				setMessages([...messages, data]);
+			} catch (error) {
+				toast.error('Error Occured!');
+			}
+		}
+	};
+
+	// useEffect(() => {
+	// 	socket = io(ENDPOINT);
+	// 	socket.emit('setup', user);
+	// 	socket.on('connected', () => setSocketConnected(true));
+	// 	socket.on('typing', () => setIsTyping(true));
+	// 	socket.on('stop typing', () => setIsTyping(false));
+
+	// 	// eslint-disable-next-line
+	// }, []);
+
+	useEffect(() => {
+		fetchMessages();
+
+		// selectedChatCompare = selectedChat;
+	}, [selectedChat]);
+
+	// useEffect(() => {
+	// 	socket.on('message recieved', (newMessageRecieved) => {
+	// 		if (
+	// 			!selectedChatCompare || // if chat is not selected or doesn't match current chat
+	// 			selectedChatCompare._id !== newMessageRecieved.chat._id
+	// 		) {
+	// 			if (!notification.includes(newMessageRecieved)) {
+	// 				setNotification([newMessageRecieved, ...notification]);
+	// 				setFetchAgain(!fetchAgain);
+	// 			}
+	// 		} else {
+	// 			setMessages([...messages, newMessageRecieved]);
+	// 		}
+	// 	});
+	// });
+
+	const typingHandler = (e) => {
+		setNewMessage(e.target.value);
+
+		// if (!socketConnected) return;
+
+		if (!typing) {
+			setTyping(true);
+			// socket.emit('typing', selectedChat._id);
+		}
+		let lastTypingTime = new Date().getTime();
+		var timerLength = 3000;
+		setTimeout(() => {
+			var timeNow = new Date().getTime();
+			var timeDiff = timeNow - lastTypingTime;
+			// if (timeDiff >= timerLength && typing) {
+			// 	socket.emit('stop typing', selectedChat._id);
+			// 	setTyping(false);
+			// }
+		}, timerLength);
 	};
 
 	const customStyles = {
@@ -75,7 +154,7 @@ const CurrentChat = ({ fetchAgain, setFetchAgain }) => {
 								width='16'
 								height='16'
 								fill='currentColor'
-								class='bi bi-caret-left'
+								className='bi bi-caret-left'
 								viewBox='0 0 16 16'
 							>
 								<path d='M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z' />
@@ -91,7 +170,7 @@ const CurrentChat = ({ fetchAgain, setFetchAgain }) => {
 											width='16'
 											height='16'
 											fill='currentColor'
-											class='bi bi-person'
+											className='bi bi-person'
 											viewBox='0 0 16 16'
 										>
 											<path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z' />
@@ -118,7 +197,7 @@ const CurrentChat = ({ fetchAgain, setFetchAgain }) => {
 											width='16'
 											height='16'
 											fill='currentColor'
-											class='bi bi-people'
+											className='bi bi-people'
 											viewBox='0 0 16 16'
 										>
 											<path d='M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z' />
@@ -140,51 +219,38 @@ const CurrentChat = ({ fetchAgain, setFetchAgain }) => {
 								</>
 							))}
 					</div>
-					<div
-					// d='flex'
-					// flexDir='column'
-					// justifyContent='flex-end'
-					// p={3}
-					// bg='#E8E8E8'
-					// w='100%'
-					// h='100%'
-					// borderRadius='lg'
-					// overflowY='hidden'
-					>
+					<div className='chat-space'>
 						{loading ? (
 							<div className='loading'>Loading</div>
 						) : (
 							<div className='messages'>
-								{/* <ScrollableChat messages={messages} /> */}
+								console.log(messages)
+								<CurrentScrollableChat messages={messages} />
 							</div>
 						)}
 
-						{/* <FormControl
-							onKeyDown={sendMessage}
-							id='first-name'
-							isRequired
-							mt={3}
-						>
+						<form id='first-name'>
 							{istyping ? (
 								<div>
-									<Lottie
+									{/* <Lottie
 										options={defaultOptions}
 										// height={50}
 										width={70}
 										style={{ marginBottom: 15, marginLeft: 0 }}
-									/>
+									/> */}
 								</div>
 							) : (
 								<></>
 							)}
-							<Input
+							<input
 								variant='filled'
 								bg='#E0E0E0'
 								placeholder='Enter a message..'
+								onKeyDown={sendMessage}
 								value={newMessage}
 								onChange={typingHandler}
 							/>
-						</FormControl> */}
+						</form>
 					</div>
 				</div>
 			) : (
---
+++ client/src/component/Chat/CurrentScrollableChat/CurrentScrollableChat.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ScrollableFeed from 'react-scrollable-feed';
+import {
+	isLastMessage,
+	isSameSender,
+	isSameSenderMargin,
+	isSameUser,
+} from '../../../config/chatConfig';
+import { ChatState } from '../../../context/chatProvider';
+
+function CurrentScrollableChat({ messages }) {
+	const { user } = ChatState();
+
+	return (
+		<div>
+			<ScrollableFeed>
+				{messages &&
+					messages.map((m, i) => (
+						<div
+							className='curr-messages'
+							style={{ color: 'var(--color-primary)', display: 'flex' }}
+							key={m._id}
+						>
+							{(isSameSender(messages, m, i, user._id) ||
+								isLastMessage(messages, i, user._id)) && (
+								<div
+									className='avatar'
+									style={{ display: 'flex', alignContent: 'end' }}
+									label={m.sender.name}
+								>
+									{console.log(m.sender)}
+									<img
+										style={{ width: '2rem', alignSelf: 'end' }}
+										name={m.sender.name}
+										src={m.sender.pfp}
+									/>
+								</div>
+							)}
+							<span
+								style={{
+									backgroundColor: `${
+										m.sender._id === user._id ? '#BEE3F8' : '#B9F5D0'
+									}`,
+									marginLeft: isSameSenderMargin(messages, m, i, user._id),
+									marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
+									borderRadius: '20px',
+									padding: '5px 15px',
+									maxWidth: '75%',
+								}}
+							>
+								{m.content}
+							</span>
+						</div>
+					))}
+			</ScrollableFeed>
+		</div>
+	);
+}
+
+export default CurrentScrollableChat;
