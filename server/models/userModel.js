397a810a67c422f386256bb7e9d4c89d95733344
Javed Habib <jh4official@gmail.com>
2023-07-30 23:34:57 +0530

    fix: Remove pfp from required - Default added - fix signup due to pfp

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
 	{
@@ -7,7 +7,6 @@ const userSchema = mongoose.Schema(
 			type: 'String',
 			require: true,
 		},
-
 		email: {
 			type: 'String',
 			require: true,
@@ -16,10 +15,8 @@ const userSchema = mongoose.Schema(
 			type: 'String',
 			require: true,
 		},
-
 		pfp: {
 			type: 'String',
-			required: true,
 			default:
 				'https://cdn-icons-png.flaticon.com/512/149/149071.png?w=826&t=st=1689422645~exp=1689423245~hmac=8322841ac71f0d730a1935c2ea057cd7634c9dc174a009d5034c58e4dd422344',
 		},
